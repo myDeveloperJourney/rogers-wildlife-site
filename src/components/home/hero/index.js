8d33317af943ada8bedcff758e5344f0b45b3bea
Clue355 <dm.carlosluevano@gmail.com>
2024-02-11 19:18:57 -0600

    edit: added className instead of class on some html elements

--- src/components/home/hero/index.js
+++ src/components/home/hero/index.js
@@ -3,11 +3,11 @@ import styles from "@/components/home/hero/Hero.module.css";
 export default function Hero() {
     return (
         <div className={styles.container_parent}>
-            <div class={styles.text_container}>
+            <div className={styles.text_container}>
                 <div className={styles.text_inner_container}>
-                    <p class={`${styles.text_header} ${styles.text_head_left}`}>Rogers</p>
-                    <p class={`${styles.text_header} ${styles.text_head_right}`}>Wildlife</p>
-                    <p class={styles.text_inc}>Rehabilitation, Inc.</p>
+                    <p className={`${styles.text_header} ${styles.text_head_left}`}>Rogers</p>
+                    <p className={`${styles.text_header} ${styles.text_head_right}`}>Wildlife</p>
+                    <p className={styles.text_inc}>Rehabilitation, Inc.</p>
                 </div>
             </div>
             <div className={styles.sub_header_container}>
