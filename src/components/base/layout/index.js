86d01bbde8cf2afe0ea412c0aa28e1550d25317c
Clue355 <dm.carlosluevano@gmail.com>
2024-02-15 20:56:16 -0600

    edit: adjusted header, nav, mobile nav

--- src/components/base/layout/index.js
+++ src/components/base/layout/index.js
@@ -4,41 +4,10 @@ import Navbar from "../navbar";
 
 export default function Layout({ children }) {
     const [isNavOpen, setNavOpen] = useState(false);
-    const [isMobileScreen, setIsMobileScreen] = useState(false);
-
-    // Debounced function to handle scroll and touchmove events
-    const handleScroll = useCallback(() => {
-        if (isNavOpen) {
-            setNavOpen(false);
-        }
-    }, [isNavOpen]);
-
-    useEffect(() => {
-        // Function to handle media query change
-        const mediaQueryChangeHandler = (e) => {
-            setIsMobileScreen(e.matches);
-        };
-
-        // Initialize and set up media query listener
-        const mediaQuery = window.matchMedia("(max-width: 1176px)");
-        mediaQuery.addEventListener("change", mediaQueryChangeHandler);
-        mediaQueryChangeHandler(mediaQuery); // Initialize state based on current screen size
-
-        // Set up event listeners for scroll and touchmove events
-        window.addEventListener("wheel", handleScroll, { passive: true });
-        window.addEventListener("touchmove", handleScroll, { passive: true });
-
-        // Cleanup event listeners
-        return () => {
-            mediaQuery.removeEventListener("change", mediaQueryChangeHandler);
-            window.removeEventListener("wheel", handleScroll);
-            window.removeEventListener("touchmove", handleScroll);
-        };
-    }, [handleScroll]);
 
     return (
         <div className="layout">
-            <Navbar isNavOpen={isNavOpen} setNavOpen={setNavOpen} isMobileScreen={isMobileScreen} />
+            <Navbar isNavOpen={isNavOpen} setNavOpen={setNavOpen} />
             {children}
             <Footer />
         </div>
