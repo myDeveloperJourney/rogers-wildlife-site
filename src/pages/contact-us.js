8bae80ae57bb35b9c485d2006f8f97676a6e8113
Daniel Scott <daniel@mydeveloperjourney.com>
2024-02-27 11:45:51 -0600

    wip: adds content rough draft for now

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -18,7 +18,58 @@ export default function Home() {
             <main className={`${styles.main} ${inter.className}`}>
                 <Hero />
                 <Layout>
-                    <h1>Contact Us</h1>
+                    <main>
+                        <h1>Contact Us</h1>
+                        <div>
+                            <section>
+                                <h3>Location</h3>
+                                <address>
+                                    <p>Rogers Wildlife Rehabilitation Center</p>
+                                    <p>1430 E. Cleveland Rd.</p>
+                                    <p>Hutchins, TX 75141</p>
+                                </address>
+                                <p><strong>Phone</strong>: 972-225-4000</p>
+                                <p><strong>Email for general Inquires</strong>: contact@rogerswildlife.org</p>
+                                <div>Social Icons</div>
+                            </section>
+                            <section>
+                                <h3>Hours</h3>
+                                <p>
+                                    <span>Open every day. Holiday hours may differ</span>
+                                    <span>Fall Hours: 9am - 4pm</span>
+                                    <span>Starting Sept. 1 - Every day</span>
+                                    <span>Summer Hours: 10am - 4pm</span>
+                                    Due to the corona virus situation we are open for bird drop offs between 10-4 every day. 
+                                    This is our busy season, and we do our best to answer everycall, 
+                                    but due to the heavy call volume and calls coming in back to back, 
+                                    you may have to call a couple of times. 
+                                    Please be patient... 
+                                    we are all volunteers and doing the best we can with the volume of birds and calls coming in. 
+                                </p>
+                            </section>
+                            <section>
+                                <h3>Directions</h3>
+                                <ol>
+                                    <li>Take I-45 South from Dallas.</li>
+                                    <li>Take exit 274 at Dowdy Ferry Road/Hutchins just South of I-20.</li>
+                                    <li>Go left under I-45 and left again on the North bound service road.</li>
+                                    <li>Turn right onto E. Cleveland Road, go one mile and we will be on the left.</li>
+                                    <li>Rogers Wildlife is located on the left side of Cleveland Road, just before you get to the Republic Services.</li>
+                                </ol>
+                            </section>
+                            <section>
+                                <h3>Far Away? Find Someone Closer</h3>
+                                <ul>
+                                    <li><a href="http://tpwd.texas.gov/huntwild/wild/rehab/list/" target="_blank">List of Texas Wildlife Rehabilitators by County</a> (Make sure to check surrounding counties too!)</li>
+                                    <li>Call the DFW Wildlife Coalition at 972-234-9453</li>
+                                    <li>Not in Texas? Contact your local animal control or parks and wildlife department for more information.</li>
+                                </ul>
+                            </section>
+                        </div>
+                        <section>
+                            <h3>Map</h3>
+                        </section>
+                    </main>
                 </Layout>
             </main>
         </>
