727343bb8e32616a95a80aa140a0471ae45408fe
Clue355 <dm.carlosluevano@gmail.com>
2024-02-26 20:20:54 -0600

    added: styling for desktop and tablet version of homepage

--- src/components/base/footer/index.js
+++ src/components/base/footer/index.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "@/components/base/footer/Footer.module.css";
 
-
 export default function Footer() {
     const linkObjs = [
         { name: "Home", path: "/" },
@@ -19,9 +18,12 @@ export default function Footer() {
 
     const links = linkObjs.map((obj) => {
         return (
-            <Link key={obj.name} href={obj.path}>
-                {obj.name}
-            </Link>
+            <>
+                <Link key={obj.name} href={obj.path}>
+                    {obj.name}
+                </Link>
+                {obj.path != "/contact-us" ? " | " : null}
+            </>
         );
     });
 
@@ -29,27 +31,32 @@ export default function Footer() {
         <div className={styles.parent_container}>
             <div className={styles.socials_container}>
                 <a>
-                    <Image src="/tiktok.svg" height={100} width={100} alt="social media image" />
+                    <Image src="/tiktok.svg" height={150} width={150} alt="social media image" />
                 </a>
                 <a>
-                    <Image src="/tiktok.svg" height={100} width={100} alt="social media image" />
+                    <Image src="/tiktok.svg" height={150} width={150} alt="social media image" />
                 </a>
                 <a>
-                    <Image src="/tiktok.svg" height={100} width={100} alt="social media image" />
+                    <Image src="/tiktok.svg" height={150} width={150} alt="social media image" />
                 </a>
             </div>
             <footer className={styles.footer}>
-                <div>{links}</div>
+                <div className={styles.footer_links}>{links}</div>
                 <div>
                     <p>Rogers Wildlife Rehabilitation Center - 1430 E Cleveland Rd, Hutchins, TX 75141</p>
                 </div>
-                <div>
+                <div className={styles.footer_contact}>
                     <p>
-                        Phone: <a href="tel:1-972-225-4000">972-225-4000</a> Email: <a href="rogerswildlife@yahoo.com">rogerswildlife@yahoo.com</a>
+                        Phone: <a href="tel:1-972-225-4000">972-225-4000</a>
+                    </p>
+                    <p>
+                        Email: <a href="rogerswildlife@yahoo.com">rogerswildlife@yahoo.com</a>
                     </p>
                 </div>
                 <div>
-                    <p>Copyright {new Date().getFullYear()} Rogers WIldlife Rehabilitation Center. All Rights Reserved.</p>
+                    <p>
+                        Copyright {new Date().getFullYear()} Rogers WIldlife Rehabilitation Center. All Rights Reserved.
+                    </p>
                 </div>
             </footer>
         </div>
