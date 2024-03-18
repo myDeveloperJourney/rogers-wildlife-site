c3231301390b4c029734537908d9565743f2845c
Clue355 <dm.carlosluevano@gmail.com>
2024-03-17 19:17:19 -0500

    edit: changed <a> element to Link

--- src/components/home/map_actions/index.js
+++ src/components/home/map_actions/index.js
@@ -2,21 +2,22 @@ import styles from "@/components/home/map_actions/map.module.css";
 import Map from "@/components/ui/map";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function MapContainer() {
     return (
         <div className={styles.parent_container}>
             <div className={styles.inner_container}>
                 <div className={styles.actions_container}>
-                    <a className={styles.margin_bottom} href="/how-to-help">
+                    <Link className={styles.margin_bottom} href="/how-to-help">
                         <Image src="/btn-volunteer.svg" height={100} width={100} alt="volunteer image link" />
-                    </a>
-                    <a className={styles.margin_bottom} href="/how-to-help">
+                    </Link>
+                    <Link className={styles.margin_bottom} href="/how-to-help">
                         <Image src="/btn-donate.svg" height={100} width={100} alt="donation image link" />
-                    </a>
-                    <a href="/contact-us">
+                    </Link>
+                    <Link href="/contact-us">
                         <Image src="/btn-contact.svg" height={100} width={100} alt="contact image link" />
-                    </a>
+                    </Link>
                 </div>
                 <Map />
             </div>
