c6acd2dc35c4d3d281057ef8f1cb735ddd9af7d0
Clue355 <dm.carlosluevano@gmail.com>
2024-05-02 21:39:34 -0500

    fix errors related to not importing images

--- src/components/base/footer/index.js
+++ src/components/base/footer/index.js
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "@/components/base/footer/Footer.module.css";
 import { SocialIcon } from "@/components/ui/social-icons";
