a00312c76d02b964ae0bf96bbe8baae6a62fc99d
Clue355 <dm.carlosluevano@gmail.com>
2024-07-18 22:01:06 -0500

    delete unecessary code

--- src/pages/how-to-help.js
+++ src/pages/how-to-help.js
@@ -131,10 +131,6 @@ export default function Home() {
                                         style={{
                                             position: "relative",
                                             border: 0,
-                                            top: 0,
-                                            left: 0,
-                                            bottom: 0,
-                                            right: 0,
                                             width: "100%",
                                             height: "83rem",
                                         }}
