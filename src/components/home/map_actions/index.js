ed7ffd9075cdd1b36b441f73445edaa01872a4fc
Daniel Scott <daniel@mydeveloperjourney.com>
2024-07-19 21:30:04 -0500

    wip: donate button now opens form

--- src/components/home/map_actions/index.js
+++ src/components/home/map_actions/index.js
@@ -9,12 +9,12 @@ export default function MapContainer() {
         <div className={styles.parent_container}>
             <div className={styles.inner_container}>
                 <div className={styles.actions_container}>
-                    <Link className={styles.margin_bottom} href="/how-to-help">
+                    <Link className={styles.margin_bottom} href="/how-to-help#volunteering">
                         <Image src="/volunteerVector.svg" height={100} width={100} alt="volunteer image link" />
                     </Link>
-                    <Link className={styles.margin_bottom} href="/how-to-help">
+                    <button type="button" className={`${styles.margin_bottom} ${styles.donate_button}`} zeffy-form-link="https://www.zeffy.com/embed/donation-form/949079ad-523d-417a-a441-e947ee0271f9?modal=true">
                         <Image src="/donateVector.svg" height={100} width={100} alt="donation image link" />
-                    </Link>
+                    </button>
                     <Link href="/contact-us">
                         <Image src="/contactVector.svg" height={100} width={100} alt="contact image link" />
                     </Link>
